f9222de8bb0ad6d52b33c2e39b393bb2597dfc60
zzaenni515 <zzaenni515@naver.com>
2018-07-28 04:48:53 +0900

    11726-2xn 타일링

---
+++ 11726/11726/main.cpp
@@ -0,0 +1,18 @@
+//11726-2xn 타일링
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int d[1001];
+	cin >> n;//2xn사각형
+
+	d[0] = 1;
+	d[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		d[i] = d[i - 1] + d[i - 2];
+		d[i] = d[i] % 10007;
+	}
+	printf("%d", d[n]);
+	return 0;
+}
